154d20feaa08fc1e04a13f793f8c2fdb5dfa6bb3
mr.drik <83313133+armandrik@users.noreply.github.com>
2021-08-20 19:18:43 +0430

    Add files via upload

---
+++ store-site/main.js
@@ -0,0 +1,6 @@
+const boxUrl = document.getElementsByClassName("box");
+for (let i = 0; i < 13; i++) {
+    boxUrl[i].addEventListener("click", function(event) {
+        window.open("https://www.amazon.com/AOC-C24G1A-Frameless-1920x1080-adjustable/dp/B08D3Y5PFZ?ref_=Oct_d_omg_d_1292115011&pd_rd_w=KTZMU&pf_rd_p=aba82a83-9bc0-4d9a-b9f0-441c9af18356&pf_rd_r=WA8GK9QGJR4G6CCVMXJP&pd_rd_r=2daa425c-d3ee-4067-8a48-51c430e3ed21&pd_rd_wg=QdLRr&pd_rd_i=B08D3Y5PFZ");
+    });
+}
